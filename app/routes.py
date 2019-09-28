fffd5c633fb28c8612990e1abc494d9877beafac
lpoulin <lpoulin@smartgistics.com>
2019-09-28 15:48:36 -0400

    added templates

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,18 @@
+from flask import render_template
 from app import app
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Hello world!"
+    user = {'username': 'guy'}
+    posts = [
+        {
+            'author': {'username': 'liam'},
+            'body': 'i like to pass the ball to the other team'
+        },
+        {
+            'author': {'username': 'dillon'},
+            'body': 'i like to drive backwards'
+        }
+    ]
+    return render_template('index.html', title='Home', user=user, posts=posts)
